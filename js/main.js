7dbe2efbda7c966bdd72d29c200fff8237cecc12
Ahsan <ahsanulhuq17@gmail.com>
2022-06-20 17:08:03 +0600

    ajax xmlhttprequest

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// function loadDate(){
+
+//     const xhr = new XMLHttpRequest();
+   
+//     xhr.onload = function(){
+
+//         const demo = document.getElementById('demo');
+//         demo.innerHTML = xhr.responseText;
+//     }
+
+//     xhr.open('GET', '../demo/demo.txt');
+
+//     xhr.send();
+// }
+
